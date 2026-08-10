f30f2264964faad22e9ea25aa56cee7422f736e6
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:25:36 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/test/java/com/google/zxing/spring/boot/client/BufferedImageLuminanceSourceTest.java
+++ src/test/java/com/google/zxing/spring/boot/client/BufferedImageLuminanceSourceTest.java
@@ -28,7 +28,7 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 /**
  * Unit tests for {@link BufferedImageLuminanceSource}.
  *
- * @author <a href="https://github.com/loong10k">[@Loong Wan]</a>
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @DisplayName("BufferedImageLuminanceSource Tests")
--- src/test/java/com/google/zxing/spring/boot/utils/BitMatrixUtilsTest.java
+++ src/test/java/com/google/zxing/spring/boot/utils/BitMatrixUtilsTest.java
@@ -35,7 +35,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Unit tests for {@link BitMatrixUtils}.
  *
- * @author <a href="https://github.com/loong10k">[@Loong Wan]</a>
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @DisplayName("BitMatrixUtils Tests")
